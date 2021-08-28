1035d3ed523ef7189e319b353e7e11d2381eb079
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-29 00:04:47 +0530

    Added program to calculate permutation

--- Permutation.java
+++ Permutation.java
@@ -1,2 +1,21 @@
-package com.dsa;public class Permutation {
+package com.dsa;
+
+import java.util.Scanner;
+
+public class Permutation {
+   static int factorial(int n){
+        int fact=1;
+        for(int i=n;i>=1;i--){
+            fact = fact*i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the values for n and r");
+        int n= input.nextInt();
+        int r= input.nextInt();
+        int permutation=factorial(n)/factorial(n-r);
+        System.out.println("THe value of "+n+"p"+r+" is "+permutation);
+    }
 }
