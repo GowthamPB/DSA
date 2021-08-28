6a28bc81a9ae436d0a2ac35fff36ecba5cda2a08
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-28 09:45:33 +0530

    Added power java program

--- Power.java
+++ Power.java
@@ -1,2 +1,18 @@
-package com.dsa;public class Power {
+package com.dsa;
+
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int result=1;
+        System.out.println("Enter the base");
+        int base= input.nextInt();
+        System.out.println("Enter the power");
+        int power= input.nextInt();
+        for(int i=0;i<power;i++){
+            result=result*base;
+        }
+        System.out.println(base+" to the power "+power+" is "+result);
+    }
 }
