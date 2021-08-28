f5e79ad4448401e049d3b04d4e18b3d8a7fd94f6
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-28 09:43:13 +0530

    Added Depreciation  java program

--- Depreciation.java
+++ Depreciation.java
@@ -1,2 +1,16 @@
-package com.dsa;public class Depreciation {
+package com.dsa;
+
+import java.util.Scanner;
+import  java.lang.Math;
+
+public class Depreciation {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the initial value,rate of depreciation,time in years");
+        int initial_value=input.nextInt();
+        float rate= input.nextFloat();
+        int time=input.nextInt();
+        float final_value= (float) (initial_value * Math.pow((1 - rate / 100),time));
+        System.out.println("The final value after depreciation is "+final_value);
+    }
 }
