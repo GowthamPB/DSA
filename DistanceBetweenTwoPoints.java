6a78785f9959ae0a326860e573c076ff32061e3a
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-28 09:44:44 +0530

    Added Distance between two points java program

--- DistanceBetweenTwoPoints.java
+++ DistanceBetweenTwoPoints.java
@@ -1,2 +1,18 @@
-package com.dsa;public class DistanceBetweenTwoPoints {
+package com.dsa;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class DistanceBetweenTwoPoints {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter values of x and y of point 1");
+        int x1=input.nextInt();
+        int y1=input.nextInt();
+        System.out.println("Enter values of x and y for point 2");
+        int x2=input.nextInt();
+        int y2=input.nextInt();
+        float distance= (float) Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("The distance between the points ("+x1+","+y1+") and ("+x2+","+y2+") is "+distance);
+    }
 }
