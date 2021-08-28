64280ce7d29d4db880096b7d2988d77d25a32d2f
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-28 12:44:31 +0530

    Added program to calculate compound interest

--- CompoundInterest.java
+++ CompoundInterest.java
@@ -1,2 +1,21 @@
-package com.dsa;public class CompoundInterest {
+package com.dsa;
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        float interest=0,total_interest=0;
+        System.out.println("Enter the principal,rate of interest and time");
+        float amount= input.nextInt();
+        float rate= input.nextFloat();
+        int time= input.nextInt();
+        for(int i=0;i<time;i++) {
+            interest=(amount*(rate/100));
+            amount=amount+interest;
+            total_interest=total_interest+interest;
+        }
+        System.out.println("The compound interest after "+time+" years is "+total_interest);
+        System.out.println("The final amount after "+time+" years is "+amount);
+    }
 }
