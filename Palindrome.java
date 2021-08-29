a95aa91826824be107f62a5a14ef42fad2530ef2
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-29 19:59:52 +0530

    Added program to check if a string is palindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -1,2 +1,22 @@
-package com.dsa;public class Palindrome {
+package com.dsa;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int flag=0;
+        System.out.println("Enter the string");
+        String string= input.next();
+        for(int i=0,j=string.length()-1;i<string.length()/2;i++,j--){
+            if(string.charAt(i)!=string.charAt(j)){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+            System.out.println(string+" is a palindrome");
+        else
+            System.out.println(string+" is not a palindrome");
+    }
 }
