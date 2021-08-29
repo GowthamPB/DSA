ce122c01088ec102e6abb7276575e0f7e1cb8560
Gowtham P B <gowthampb.is19@sahyadri.edu.in>
2021-08-29 19:59:02 +0530

    Added program to check if a number is perfect or not

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,2 +1,21 @@
-package com.dsa;public class PerfectNumber {
+package com.dsa;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int sum=1;
+        System.out.println("Enter a number");
+        int number= input.nextInt();
+        for(int i=2;i<=number/2;i++){
+            if(number%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==number)
+            System.out.println(number+" is a perfect number");
+        else
+            System.out.println(number+" is not a perfect number");
+    }
 }
