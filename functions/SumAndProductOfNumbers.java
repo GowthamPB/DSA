736dee35b946ce764920a35fb28c3a6f408eeb3c
GowthamPB <gowthampb.is19@sahyadri.edu.in>
2021-12-22 23:59:30 +0530

    Addeda program

---
+++ functions/SumAndProductOfNumbers.java
@@ -0,0 +1,23 @@
+//Subtract the Product and Sum of Digits of an Integer
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+//https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+public class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1, sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            product *= rem;
+            sum += rem;
+            n = n / 10;
+        }
+        return (product - sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        Solution sol = new Solution();
+        int result = sol.subtractProductAndSum(num);
+        System.out.println(result);
+    }
+}
