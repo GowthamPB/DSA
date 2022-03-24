fb182981e5f7afba2547ac60096f545eee0228de
Gowtham P B <67544642+GowthamPB@users.noreply.github.com>
2022-03-24 23:14:14 +0530

    Time: 1 ms (100.00%), Space: 54.7 MB (48.47%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count=0;
+        for(int i:nums){
+            if(i!=0){
+                nums[count]=i;
+                count++;
+            }
+        }
+        for(int j=count;j<nums.length;j++){
+            nums[j]=0;
+        }
+    }
+}
