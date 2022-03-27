f4713579a584232eee801ad3840d5243d91e73c7
Gowtham P B <67544642+GowthamPB@users.noreply.github.com>
2022-03-27 09:25:09 +0530

    Time: 7 ms (36.89%), Space: 38.6 MB (94.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int fib(int n) {
-        int fibonacci;
         if(n==0 || n==1){
             return n;
         }
         else{
-            return fibonacci=fib(n-1)+fib(n-2);
+            return fib(n-1)+fib(n-2);
         }
     }
 }
