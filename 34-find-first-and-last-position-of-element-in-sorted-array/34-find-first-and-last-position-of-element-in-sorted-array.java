b9c67262c8bb7985df479dcf0a3ad6eb8edfd821
Gowtham P B <67544642+GowthamPB@users.noreply.github.com>
2022-10-02 12:35:11 +0530

    Time: 1 ms (42.72%), Space: 47.4 MB (38.85%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] searchRange(int[] v, int x) {
         int[] ans=new int[2];
-        int start=0,end=v.length-1;        
-        ans[0]=bins(v,x,start,end,true);
-        ans[1]=bins(v,x,start,end,false);
+            
+        ans[0]=bins(v,x,true);
+        ans[1]=bins(v,x,false);
         
         return ans;
     }
-    public static int bins(int v[],int x,int start,int end,boolean val){
+    public static int bins(int v[],int x,boolean val){
        int mid;
        int ans=-1;
+       int start=0,end=v.length-1;  
        while(start<=end){
            mid=start+(end-start)/2;
            if(x<v[mid]){
@@ -20,10 +21,8 @@ class Solution {
                ans=mid;
                if(val){
                    end=mid-1;
-                   bins(v,x,start,end,val);
                }else{
                    start=mid+1;
-                   bins(v,x,start,end,val);
                }
                
            }
