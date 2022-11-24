22f7b91e88370b3e8b90882e138ce80fbc6d22c1
Gowtham P B <67544642+GowthamPB@users.noreply.github.com>
2022-11-25 00:24:35 +0530

    Time: 4 ms (82.86%), Space: 41.7 MB (68.25%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s="1";
+        for(int i=1;i<n;i++){
+            s=freqCount(s);
+        }
+        return s;
+    }
+    static String freqCount(String str){
+        StringBuilder sb=new StringBuilder();
+        char prev=str.charAt(0);
+        int count=1;
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==prev){
+                count++;
+            }else{
+                sb.append(count);
+                sb.append(prev);
+                prev=str.charAt(i);
+                count=1;
+            }
+        }
+        sb.append(count);
+        sb.append(prev);
+        return sb.toString();
+    }
+}
